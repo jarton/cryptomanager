1c35cddd570f571ac6cb643ec45305aff1c533e3
jarton <jdrton@icloud.com>
2019-01-19 18:47:57 +0100

    Added a testcomponent

---
+++ src/Components/TestCompnent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Test() {
+    return (
+      <div> 
+      <p>test asdf  </p>
+      <p>test asdf  </p>
+      </div>
+    );
+}
--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import Test from './Components/TestCompnent.jsx';
 
 class App extends Component {
   render() {
     return (
       <div> 
-      <p> Test </p>
+      <Test/>
       </div>
     )
   }
